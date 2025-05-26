dc7683330aaa44118481b696f9ce96ef65c87574
brunetto323 <bukovic.brunetto@gmail.com>
2025-05-26 15:10:33 +0000

    1.4

--- main.cpp
+++ main.cpp
@@ -4,16 +4,15 @@
 #include <cstring>
 #include <ctime>
 #include <fstream>
-#include <cstdio>
+ 
 
 using namespace std;
 
-const int VELICINA = 10;
-const int MAX_IGRACA = 4;
-const int MAX_IME = 50;
-
-int Crveni_brojevi[] = {1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36};
-int Crni_brojevi[] = {2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35};
+#define MAX_IGRACA 4
+#define MAX_IME 50
+#define VELICINA 10
+#define VELICINA_CRVENI 18
+#define VELICINA_CRNI 18
 
 struct Igrac
 {
@@ -41,16 +40,16 @@ void prikazRuleta(int broj, int polje[VELICINA][VELICINA])
         "31", "32", "33", "34", "35", "36"};
 
     system("clear");
-    printf("\nRulet je pao na broj: [%s]\n", kotac[broj]);
+    cout << "\nRulet je pao na broj: [" << kotac[broj] << "]\n";
     polje[VELICINA / 2][VELICINA / 2] = broj;
 
     for (int i = 0; i < VELICINA; i++)
     {
         for (int j = 0; j < VELICINA; j++)
         {
-            printf("%2d ", polje[i][j]);
+            cout << polje[i][j] << " ";
         }
-        printf("\n");
+        cout << "\n";
     }
 }
 
@@ -72,3 +71,181 @@ void rotirajPolje(int polje[VELICINA][VELICINA])
         }
     }
 }
+
+void prikazNaslov()
+{
+    cout << "\033[0;34m";
+    cout << "               Boki i Brunci\n";
+    cout << "\033[1;31m";
+    cout << "     ______            _          _     _    \n ";
+    cout << "    |  __ \\          | |        | |   | | \n";
+    cout << "     | |__) |  _   _  | |   ___  | |_  | |_      \n ";
+    cout << "    |  _  /  | | | | | |  / _ \\ | __| | __|  \n ";
+    cout << "    | | \\ \\  | |_| | | | |  __/ | |_  | |_    \n";
+    cout << "     |_|  \\_\\  \\____| |_|  \\___|  \\__|  \\__| \n";
+    cout << "\033[0m";
+}
+
+void postaviOklade(Igrac *igrac)
+{
+    int unosIzbora = 0;
+
+    do
+    {
+        cout << "Igrač " << igrac->ime << ", odaberite tip oklade:\n";
+        cout << "1. Pojedinačni broj (0-36)\n";
+        cout << "2. Parni brojevi\n";
+        cout << "3. Neparni brojevi\n";
+        cout << "4. Crveni brojevi\n";
+        cout << "5. Crni brojevi\n";
+        cout << "Unesite izbor (1-5): ";
+        cin >> unosIzbora;
+
+        if (unosIzbora < 1 || unosIzbora > 5)
+        {
+            cout << "Krivi unos. Molimo unesite broj između 1 i 5.\n";
+        }
+    } while (unosIzbora < 1 || unosIzbora > 5);
+
+    igrac->tipOklade = unosIzbora;
+
+    if (igrac->tipOklade == 1)
+    {
+        cout << "Unesite broj na koji želite da se kladite (0-36): ";
+        cin >> igrac->brojOklade;
+        while (igrac->brojOklade < 0 || igrac->brojOklade > 36)
+        {
+            cout << "Nevažeći broj! Molimo unesite broj između 0 i 36: ";
+            cin >> igrac->brojOklade;
+        }
+    }
+
+    cout << "Unesite iznos novca koji želite da uložite: ";
+    cin >> igrac->iznosOklade;
+    while (igrac->iznosOklade > igrac->novac || igrac->iznosOklade <= 0)
+    {
+        if (igrac->iznosOklade <= 0)
+            cout << "Iznos mora biti pozitivan. Pokušajte ponovo: ";
+        else
+            cout << "Nemate dovoljno novca! Pokušajte s manjim iznosom: ";
+        cin >> igrac->iznosOklade;
+    }
+}
+
+bool jeCrveni(int broj)
+{
+    for (int i = 0; i < VELICINA_CRVENI; i++)
+    {
+        if (Crveni_brojevi[i] == broj)
+            return true;
+    }
+    return false;
+}
+
+bool jeCrni(int broj)
+{
+    for (int i = 0; i < VELICINA_CRNI; i++)
+    {
+        if (Crni_brojevi[i] == broj)
+            return true;
+    }
+    return false;
+}
+
+void provjeriOklade(Igrac *igrac, int rezultat)
+{
+    bool pogodak = false;
+
+    switch (igrac->tipOklade)
+    {
+    case 1: // Pojedinačni broj
+        if (igrac->brojOklade == rezultat)
+        {
+            igrac->novac += igrac->iznosOklade * 36;
+            pogodak = true;
+            cout << "Čestitamo " << igrac->ime << "! Pogodili ste broj " << rezultat << "!\n";
+        }
+        break;
+    case 2: // Parni brojevi
+        if (rezultat != 0 && rezultat % 2 == 0)
+        {
+            igrac->novac += igrac->iznosOklade * 1;
+            pogodak = true;
+            cout << "Čestitamo " << igrac->ime << "! Pogodili ste parni broj " << rezultat << "!\n";
+        }
+        break;
+    case 3: // Neparni brojevi
+        if (rezultat % 2 == 1)
+        {
+            igrac->novac += igrac->iznosOklade * 1;
+            pogodak = true;
+            cout << "Čestitamo " << igrac->ime << "! Pogodili ste neparni broj " << rezultat << "!\n";
+        }
+        break;
+    case 4: // Crveni brojevi
+        if (jeCrveni(rezultat))
+        {
+            igrac->novac += igrac->iznosOklade * 1;
+            pogodak = true;
+            cout << "Čestitamo " << igrac->ime << "! Pogodili ste crveni broj " << rezultat << "!\n";
+        }
+        break;
+    case 5: // Crni brojevi
+        if (jeCrni(rezultat))
+        {
+            igrac->novac += igrac->iznosOklade * 1;
+            pogodak = true;
+            cout << "Čestitamo " << igrac->ime << "! Pogodili ste crni broj " << rezultat << "!\n";
+        }
+        break;
+    }
+    if (!pogodak)
+    {
+        igrac->novac -= igrac->iznosOklade;
+        cout << igrac->ime << ", nažalost niste pogodili. Izgubili ste " << igrac->iznosOklade << ".\n";
+    }
+}
+////
+void spremiIgrace(const Igrac igraci[], int brojIgraca, const char *filename)
+{
+    ofstream f(filename);
+    if (!f)
+    {
+        cout << "Ne mogu otvoriti datoteku za spremanje.\n";
+        return;
+    }
+    for (int i = 0; i < brojIgraca; i++)
+    {
+        f << igraci[i].ime << " " << igraci[i].novac << "\n";
+    }
+    f.close();
+}
+///
+
+void ucitajIgrace(Igrac igraci[], int &brojIgraca, const char *filename)
+{
+    ifstream f(filename);
+    if (!f)
+    {
+        cout << "Ne mogu otvoriti datoteku za učitavanje.\n";
+        return;
+    }
+    brojIgraca = 0;
+    while (f >> igraci[brojIgraca].ime >> igraci[brojIgraca].novac)
+    {
+        igraci[brojIgraca].tipOklade = 0;
+        igraci[brojIgraca].brojOklade = 0;
+        igraci[brojIgraca].iznosOklade = 0;
+        brojIgraca++;
+        if (brojIgraca >= MAX_IGRACA)
+            break;
+    }
+    f.close();
+}
+//
+int main()
+{
+    srand(time(nullptr));
+    Igrac igraci[MAX_IGRACA];
+    int brojIgraca = 0;
+};
